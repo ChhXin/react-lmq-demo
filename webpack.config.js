9c6fda87ba27e4ef31b38626e8ae44854b1b9a0a
ChhXin <chh.xin@gmail.com>
2019-01-29 12:00:12 +0800

    init code

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: '(none)',
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'public/build'),
+    filename: '[id].bundle.js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/build/'
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ["stage-0", 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader", // compiles Less to CSS
+          options: {
+            javascriptEnabled: true
+          }
+        }]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html'
+    },
+    index: 'index.html',
+    contentBase: path.join(__dirname, 'public'),
+    compress: true, // enable gzip compression
+    https: false, // true for self-signed, object for cert authority
+  },
+};
